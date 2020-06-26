429373efe5370ef95260e6c548fcfa2a3c8c67ca
Donald Cameron <drc85@live.co.uk>
2020-06-26 19:52:21 +0100

    added african and antartic animals to seeds

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -2,24 +2,189 @@ use vue_project;
 db.dropDatabase();
 
 db.animals.insertMany([
+
+  // {
+  //   "name": "string",
+  //   "speed": integer or float mph,
+  //   "weight": integer or float kg,
+  //   "height": integer or float cm,
+  //   "population": integer
+  // },
   {
     "name": "African Elephant",
     "speed": 25,
     "weight": 6000,
+    "height": 320,
     "population": 415000
   },
   {
     "name": "Cheetah",
     "speed": 80,
     "weight": 65,
+    "height": 90,
     "population": 7100
   },
   {
     "name": "Giraffe",
     "speed": 37,
     "weight": 800,
+    "height": 610,
     "population": 97500
-  }
+  },
+  {
+    "name": "Lion",
+    "speed": 50,
+    "weight": 190,
+    "height": 120,
+    "population": 20000
+  },
+  {
+    "name": "White Rhinoceros",
+    "speed": 31,
+    "weight": 2300,
+    "height": 190,
+    "population": 18000
+  },
+  {
+    "name": "Leopard",
+    "speed": 36,
+    "weight": 31,
+    "height": 70,
+    "population": 700000
+  },
+  {
+    "name": "Mountain Gorilla",
+    "speed": 25,
+    "weight": 150,
+    "height": 180,
+    "population": 1063
+  },
+  {
+    "name": "Cape Buffalo",
+    "speed": 35,
+    "weight": 870,
+    "height": 150,
+    "population": 900000
+  },
+  {
+    "name": "Emperor Penguin",
+    "speed": 5,
+    "weight": 23,
+    "height": 130,
+    "population": 250000
+  },
+  {
+    "name": "Walrus",
+    "speed": 21,
+    "weight": 1000,
+    "height": 240,
+    "population": 129000
+  },
+  {
+    "name": "Wandering Albatross",
+    "speed": 67,
+    "weight": 7,
+    "height": 360,
+    "population": 20100
+  },
+  {
+    "name": "Chimpanzee",
+    "speed": 25,
+    "weight": 60,
+    "height": 150,
+    "population": 300000
+  },
+  {
+    "name": "Giant Panda",
+    "speed": 20,
+    "weight": 100,
+    "height": 90,
+    "population": 1864
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
+  {
+    "name": "",
+    "speed": ,
+    "weight": ,
+    "height": ,
+    "population":
+  },
 ]);
 
 
