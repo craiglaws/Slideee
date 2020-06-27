39e606e0c88a09209595ba732bce92dc51fd2709
samcassie <samgcassie@gmail.com>
2020-06-27 18:24:09 +0100

    sorted the slider bar

--- client/src/components/QuestionsDisplay.vue
+++ client/src/components/QuestionsDisplay.vue
@@ -3,10 +3,12 @@
     <div>
         {{ findCorrectAnswer() }}
 
+        <h1>{{ correctAnswer }}</h1>
+
         <h3>{{ selectedQuestion.question }} {{ selectedAnimal.name }}</h3>
 
         <div class="slidecontainer">
-          <input type="range" min="1" :max="" value="50" class="slider" id="slider" v-model="guessAnswer">
+          <input type="range" min="0" :max="getMaxValue()" :step="(getMaxValue()/100)" class="slider" id="slider" v-model="guessAnswer">
         </div>
 
     </div>
@@ -19,8 +21,10 @@
     export default {
         name: 'questions-display',
         data() {
-            guessAnswer: null,
-            correctAnswer: null
+            return {
+                guessAnswer: null,
+                correctAnswer: null
+            }
         },
         props: {
             selectedAnimal: Object,
@@ -28,14 +32,54 @@
         },
         methods: {
             findCorrectAnswer(){
-                this.correctAnswer = this.selectedAnimal[selectedQuestion.value]
+                this.correctAnswer = this.selectedAnimal[this.selectedQuestion.value]
+            },
+            getMaxValue(){
+                let max_value = (this.correctAnswer * (1.3 + (Math.random() * 0.3)))
+                if (max_value < 100) {
+                    max_value = Math.ceil(max_value/100)*100;
+
+                } else if (max_value >= 100 && max_value < 1000) {
+                    max_value = Math.ceil(max_value/1000)*1000;
+
+                } else if (max_value >= 1000 && max_value < 10000) {
+                    max_value = Math.ceil(max_value/10000)*10000;
+
+                } else if (max_value >= 10000 && max_value < 100000) {
+                    max_value = Math.ceil(max_value/100000)*100000;
+
+                } else if (max_value >= 100000 && max_value < 1000000) {
+                    max_value = Math.ceil(max_value/1000000)*1000000;
+
+                } else if (max_value >= 1000000 && max_value < 10000000) {
+                    max_value = Math.ceil(max_value/10000000)*10000000;
+                }
+
+                return max_value
             }
-
-            // getMaxValue(){
-            //     this.SelectedAnimal[]
-            // }
+            // getMinValue(){
+            //     let min_value = (this.correctAnswer * ( 0.7 - (Math.random() * 0.3)))
+            //     if (min_value < 100) {
+            //         min_value = Math.ceil(min_value/100)*10;
+            //
+            //     } else if (min_value >= 100 && min_value < 1000) {
+            //         min_value = Math.ceil(min_value/1000)*1000;
+            //
+            //     } else if (min_value >= 1000 && min_value < 10000) {
+            //         min_value = Math.ceil(min_value/10000)*10000;
+            //
+            //     } else if (min_value >= 10000 && min_value < 100000) {
+            //         min_value = Math.ceil(min_value/100000)*100000;
+            //
+            //     } else if (min_value >= 100000 && min_value < 1000000) {
+            //         min_value = Math.ceil(min_value/1000000)*1000000;
+            //
+            //     } else if (min_value >= 1000000 && min_value < 10000000) {
+            //         min_value = Math.ceil(min_value/10000000)*10000000;
+            //     }
+            //
+            //     return min_value
         }
-
     }
 
 
